59b8842cb37599b18ff8396746b6d7152c5500a7
Bytangle <dev.bytangle@gmail.com>
2023-01-08 22:57:10 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Recipe } = require("./recipe");
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+
+// root path
+app.get('/', (_, res) => {
+    res.end("Hello from Test-Ci")
+})
+
+// endpoint to get prepare recipes
+app.get("/recipe/:id", async (req, res) => {
+    let { id } = req.params;
+
+    let recipe = new Recipe(id);
+    await recipe.hydrate();
+
+    res.json(recipe)
+})
+
+// start server
+server.listen(PORT, HOST, () => {
+    console.log(`Server running at ${HOST}:${PORT}`);
+})
